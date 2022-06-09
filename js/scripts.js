c72c5cbac5f5f56fb9a3921aa4b82f1a97c42e12
KostyaUA <pm.shcherbyna.k@gmail.com>
2022-06-09 15:15:13 +0300

    quality_edits

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,21 @@ $(document).ready(function(){
             }
         }
     });
+    $('.slide2').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:4
+            }
+        }
+    });
 });
+
